c7439297a31408cb525bf0d460ec32a3b97f38ab
pablo.kraan <pablo.kraan@gmail.com>
2017-02-15 15:48:56 -0800

    Embedded
    _ Adding embedded-api

---
+++ modules/embedded-api/src/main/java/org/mule/runtime/module/embedded/api/ArtifactInfo.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.runtime.module.embedded.api;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
+public class ArtifactInfo {
+
+  private final List<URI> configs;
+  private final URL classesFolder;
+  private final URL pomFile;
+
+  public ArtifactInfo(List<URI> configs, URL classesFolder, URL pomFile) {
+    this.configs = configs;
+    this.classesFolder = classesFolder;
+    this.pomFile = pomFile;
+  }
+
+  public List<URI> getConfigs() {
+    return configs;
+  }
+
+  public URL getClassesFolder() {
+    return classesFolder;
+  }
+
+  public URL getPomFile() {
+    return pomFile;
+  }
+}
